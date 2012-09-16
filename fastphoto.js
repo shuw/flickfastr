d915842e40537e57c1b5ab0500c892c8c64b947c
Shu Wu <shuwu83@gmail.com>
2012-09-16 00:40:17 -0700

    move options to top

--- fastphoto.js
+++ fastphoto.js
@@ -1,23 +1,23 @@
 jQuery.fn.fastphoto = function(identifier, api_key, options) {
+  options = $.extend({
+    photo_size: 'b',                  // Sizes defined here: http://www.flickr.com/services/api/misc.urls.html
+    identifier_type: 'user_name'      // ['user_id', 'user_name'] supported
+  }, options)
+
   FLICKR_IMG_URL = 'http://farm9.staticflickr.com/{server}/{id}_{secret}_{size}.jpg'
   FLICKR_PHOTO_URL = 'http://www.flickr.com/photos/{user_id}/{id}'
   FLICKR_API_URL = 'http://api.flickr.com/services/rest/?format=json'
-  PHOTO_HTML = '<a id="photo/{id}" class="photo" target="_blank" href="{href}">\
-      <img title="{title}" src="{src}"></img>\
-      <span class="title">{title}</span>\
-    </a>'
+  PHOTO_HTML = '<a id="photo/{id}" class="photo" target="_blank" href="{href}"><img title="{title}" src="{src}"></img><div class="title">{title}</div></a>'
+
+  $el = this, $window = $(window), $document = $(document)
+  user_id = null, current_page = 1, max_pages = null, loading_photos = false
 
   substitute = function(str, sub) {
     return str.replace(/\{(.+?)\}/g, function($0, $1) { return $1 in sub ? sub[$1] : $0; })
   }
 
   flickr_get = function(method, params, callback) {
-    url = FLICKR_API_URL + "&" + $.param($.extend({
-      "api_key": api_key,
-      "method": method,
-      "per_page": 10
-    }, params))
-
+    url = FLICKR_API_URL + "&" + $.param($.extend({ api_key: api_key, method: method, per_page: 10 }, params))
     $.ajax(url, {dataType: "jsonp", jsonpCallback: "jsonFlickrApi", cache: true, success: callback})
   }
 
@@ -49,9 +49,10 @@ jQuery.fn.fastphoto = function(identifier, api_key, options) {
     }
   }
 
-  start_for_user = function(id) {
-    user_id = id, maintain_runway = function() {
-      runway = $document.height() - ($(window).scrollTop() + $(window).height())
+  start_for_user_id = function(id) {
+    user_id = id
+    maintain_runway = function() {
+      runway = $document.height() - $window.scrollTop() - $window.height()
       if (runway < $window.height() * 2) {
         load_photos()
       }
@@ -61,24 +62,16 @@ jQuery.fn.fastphoto = function(identifier, api_key, options) {
     load_photos()
   }
 
-  options = $.extend({
-    photo_size: 'b',                  // Sizes defined here: http://www.flickr.com/services/api/misc.urls.html
-    identifier_type: 'user_name'      // ['user_id', 'user_name'] supported
-  }, options)
-
-  $el = this, $window = $(window), $document = $(document)
-  user_id = null, current_page = 1, max_pages = null, loading_photos = false
-
   switch (options.identifier_type) {
     case 'user_id':
-      start_for_user(identifier)
+      start_for_user_id(identifier)
       break
     case 'user_name':
       url = flickr_get('flickr.people.findByUsername', {
         username: identifier
       }, function(data) {
         if (data.user.id) {
-          start_for_user(data.user.id)
+          start_for_user_id(data.user.id)
         } else {
           $el.text("could not find user name: " + identifier)
         }
