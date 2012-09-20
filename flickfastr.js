60d91bacda58e36d32ab5387d5fdfdaf5d28e82b
Shu Wu <shuwu83@gmail.com>
2012-09-20 10:08:33 -0700

    stop polluting global namespace

--- flickfastr.js
+++ flickfastr.js
@@ -7,20 +7,23 @@ jQuery.fn.flickfastr = function(identifier, api_key, options) {
     identifier_type: 'user_name'      // ['user_id', 'user_name'] supported
   }, options)
 
-  FLICKR_IMG_URL = 'http://farm9.staticflickr.com/{server}/{id}_{secret}_{size}.jpg'
-  FLICKR_PHOTO_URL = 'http://www.flickr.com/photos/{user_id}/{id}'
-  FLICKR_API_URL = 'http://api.flickr.com/services/rest/?format=json'
-  PHOTO_HTML = '<a id="photo/{id}" class="photo" target="_blank" href="{href}"><img title="{title}" src="{src}"></img><div class="title">{title}</div></a>'
+  var FLICKR_IMG_URL = 'http://farm9.staticflickr.com/{server}/{id}_{secret}_{size}.jpg'
+  var FLICKR_PHOTO_URL = 'http://www.flickr.com/photos/{user_id}/{id}'
+  var FLICKR_API_URL = 'http://api.flickr.com/services/rest/?format=json'
+  var PHOTO_HTML = '<a id="photo/{id}" class="photo" target="_blank" href="{href}"><img title="{title}" src="{src}"></img><div class="title">{title}</div></a>'
 
-  $el = this, $window = $(window), $document = $(document)
-  user_id = null, current_page = 1, max_pages = null, loading_photos = false
+  var $el = this, $window = $(window), $document = $(document)
+  var user_id = null
+  var current_page = 1
+  var max_pages = null
+  var loading_photos = false
 
   substitute = function(str, sub) {
     return str.replace(/\{(.+?)\}/g, function($0, $1) { return $1 in sub ? sub[$1] : $0; })
   }
 
   flickr_get = function(method, params, callback) {
-    url = FLICKR_API_URL + "&" + $.param($.extend({ api_key: api_key, method: method, per_page: 10 }, params))
+    var url = FLICKR_API_URL + "&" + $.param($.extend({ api_key: api_key, method: method, per_page: 10 }, params))
     $.ajax(url, {dataType: "jsonp", jsonpCallback: "jsonFlickrApi", cache: true, success: callback})
   }
 
@@ -70,7 +73,7 @@ jQuery.fn.flickfastr = function(identifier, api_key, options) {
       start_for_user_id(identifier)
       break
     case 'user_name':
-      url = flickr_get('flickr.people.findByUsername', {
+      flickr_get('flickr.people.findByUsername', {
         username: identifier
       }, function(data) {
         if (data.user.id) {
